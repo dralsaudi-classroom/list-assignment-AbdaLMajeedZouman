4a92201ba0f445fe50dc8d1298d5ff4288539f07
AbdaLMajeedZouman <444101580@student.ksu.edu.sa>
2024-09-27 04:17:02 +0300

    Update ListTester.java
    implement as a user reverseCopy() function

--- src/main/java/com/example/project/ListTester.java
+++ src/main/java/com/example/project/ListTester.java
@@ -10,13 +10,23 @@ public class ListTester {
         // B, C, D, E, A, circularShiftLeft(l, 2) results in C, D, E, A, B.
     }
     public static <T> void reverseCopy(DLL<T> l1, DLL<T> l2)
-    {
-        throw new UnsupportedOperationException("Not supported yet.");
-        // Write the method reverseCopy, user of DoubleLinkedList, which copies the elements
-        // of l1 to l2 in reverse order. The list l1 must not change. Assume that l2 is empty.
-        // The method signature is public static <T> void reverseCopy(DoubleLinkedList<T> l1,
-        // DoubleLinkedList<T> l2).
-        // Example 4.1. If l1 : A ↔ B ↔ C ↔ D, then calling reverseCopy(l1, l2) results in
-        // l2 : D ↔ C ↔ B ↔ A.
-    }
-}
+  	    {
+		   if(!l1.empty()) {
+			   
+			   
+		   while(!l1.last()) {
+			   l1.findNext();
+		   }
+		   while(!l1.first()) {
+			   l2.insert(l1.retrieve());
+			   l1.findPrevious();
+		   
+			   
+		   }
+		   l2.insert(l1.retrieve());
+		   l2.findFirst();
+		   
+		   
+		   
+	     
+	    }}}
